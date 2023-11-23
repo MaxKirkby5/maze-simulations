73350f421b176b584c2c2e777373d10ebb9339e9
MaxKirkby5 <maxkirkby1@outlook.com>
2023-11-23 23:49:40 +0000

    MK complete maze builder + optimiser

--- data/maze_construction.py
+++ data/maze_construction.py
@@ -1,161 +1,108 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import random
-import pickle
-import math
+import itertools
 import numpy as np
 from scipy.spatial.distance import euclidean
 
-# generate a two-dimensional grid graph (7x7)
-G = nx.grid_2d_graph(7, 7)
-
-# set parameters of graph generation and grid size/position
-num_graphs = 100
-grid_size = 7
-pos = dict( (n, n) for n in G.nodes() )
-
-# function to store and load graphs
-def store_graph(graph, file_name) :
-    with open(file_name, 'wb') as f:
-        pickle.dump(graph, f)
-
-def load_graph(file_name) :
-    with open(file_name, 'rb') as f:
-        return pickle.load(f)
-
 # function to remove random edges from the 7x7 grid graph + maintain connectivity
-def random_maze(G):
-    edges_removed = 0
-    limit = 24 # come back to = can we use minimum of n-1 edges (to maintain connectivity)?
-    while edges_removed < limit:
-        node = random.choice(list(G.nodes))
-        get_neighbors = list(G.neighbors(node))
-        if get_neighbors:  
-            rand_neighbor = random.choice(get_neighbors)
-            if G.has_edge(node, rand_neighbor):
-                G.remove_edge(node, rand_neighbor)
-                if nx.is_connected(G):
-                    edges_removed += 1
-                else:
-                    G.add_edge(node, rand_neighbor)
-random_maze(G)
+def random_maze():
+    graph = nx.grid_2d_graph(7, 7)
+    edges = list(graph.edges)
+    num_edges_to_remove = random.randint(0, 28)
+    for _ in range(num_edges_to_remove):
+        graph.remove_edge(*random.choice(edges))
 
+    return graph
+    
 # function for assessing outer edge counts 
-def outer_edge_count(G):
+def outer_edge_count(graph):
     outer_edge_count = 0
-    for ((x1, y1), (x2, y2)) in G.edges():
-        # Check if both nodes of the edge are on the perimeter
-        if (x1 == 0 or x1 == grid_size - 1 or y1 == 0 or y1 == grid_size - 1) and \
-           (x2 == 0 or x2 == grid_size - 1 or y2 == 0 or y2 == grid_size - 1):
+    for ((x1, y1), (x2, y2)) in graph.edges():
+        if (x1 == 0 or x1 == 7 - 1 or y1 == 0 or y1 == 7 - 1) and \
+           (x2 == 0 or x2 == 7 - 1 or y2 == 0 or y2 == 7 - 1):
             outer_edge_count += 1
     return outer_edge_count
 
-# create coordinate system for overlayed graph 
-def merge(list1, list2):
-    merged_list = []
-    for i in list1 :
-        for j in list2:
-            merged_list.append((i, j))
-    return merged_list
+# create coordinate system for inverse graph using cartesian product of two lists
+def create_inverse_graph(graph):
+    inverse_graph = nx.Graph()
+    nodes = list(itertools.product(np.arange(0.5, 6.0, 1.0), repeat=2))
+    inverse_graph.add_nodes_from(nodes)
 
-m_nodes = [0.5, 1.5, 2.5, 3.5, 4.5, 5.5]
-n_nodes = [0.5, 1.5, 2.5, 3.5, 4.5, 5.5]
-merged_nodes = merge(m_nodes, n_nodes)
-G2 = nx.Graph()
-
-def create_G2():
- for i in m_nodes:
-    for j in n_nodes:
-        G2.add_node((i, j))
-create_G2()
-
-def conditional_G2_graph(G, G2):
+    # run edge algorithm on inverse graph to search for loops
     for x1 in range(0, 6):
         for y1 in range(0, 6):
             node_G2 = (x1 + 0.5, y1 + 0.5)
             right_neighbor = (x1 + 1.5, y1 + 0.5)
             upper_neighbor = (x1 + 0.5, y1 + 1.5)
             
-            # Corresponding nodes in G that would intersect with an edge in G2
-            if x1 < 5:  # Check to the right, but not for the last column
-                if not G.has_edge((x1 + 1, y1), (x1 + 1, y1 + 1)):
-                    G2.add_edge(node_G2, right_neighbor)
+            if x1 < 5:  
+                if not graph.has_edge((x1 + 1, y1), (x1 + 1, y1 + 1)):
+                    inverse_graph.add_edge(node_G2, right_neighbor)
             
-            if y1 < 5:  # Check above, but not for the top row
-                if not G.has_edge((x1, y1 + 1), (x1 + 1, y1 + 1)):
-                    G2.add_edge(node_G2, upper_neighbor)
+            if y1 < 5: 
+                if not graph.has_edge((x1, y1 + 1), (x1 + 1, y1 + 1)):
+                    inverse_graph.add_edge(node_G2, upper_neighbor)
 
-# Call the function with the two graphs
-conditional_G2_graph(G, G2)
+    # plot graph and inverse graph
+    pos2 = {(x, y): (x, y) for x, y in inverse_graph.nodes()}
+    pos = {node: node for node in graph.nodes()}
+    merged_graph = nx.compose(graph, inverse_graph)
+    combined_pos = pos.copy()
+    combined_pos.update(pos2)
 
-open_clusters = []
-closed_clusters = []
+    colors_g = ['blue' for node in graph.nodes()]
+    sizes_g = [300 for node in graph.nodes()]
+    colors_g2 = ['red' for node in inverse_graph.nodes()]
+    sizes_g2 = [100 for node in inverse_graph.nodes()]
+    combined_colors = colors_g + colors_g2
+    combined_sizes = sizes_g + sizes_g2
 
-def checking_paths(graph):
-    clusters = list(nx.connected_components(G2))
+    nx.draw(merged_graph, combined_pos, with_labels=True, node_color=combined_colors, node_size=combined_sizes, font_size=10)
+    plt.show()
+    
+    return inverse_graph
+
+def checking_paths(graph, inverse_graph):
+    open_clusters = []
+    closed_clusters = []
+    clusters = list(nx.connected_components(inverse_graph))
     filtered_clusters = sorted([cluster for cluster in clusters if len(cluster) > 2], key=len, reverse=True)
+    
     for cluster in filtered_clusters:
+        open_flag = False
         for (x1, y1) in cluster:
-            if x1 == 0.5 :
-                if not G.has_edge((x1 - 0.5, y1+0.5), (x1-0.5, y1-0.5)):
-                    open_clusters.append(cluster)
-            if x1 == 5.5 :
-                if not G.has_edge((x1 + 0.5, y1+0.5), (x1+0.5, y1-0.5)):
-                    open_clusters.append(cluster)
-            if y1 == 5.5 :
-                if not G.has_edge((x1 - 0.5, y1+0.5), (x1+0.5, y1+0.5)):
-                    open_clusters.append(cluster)
-            if y1 == 0.5 :
-                if not G.has_edge((x1 - 0.5, y1-0.5), (x1+0.5, y1-0.5)):
-                    open_clusters.append(cluster)
-    for element in filtered_clusters :
-        if element not in open_clusters :
-            closed_clusters.append(element)
+            if x1 == 0.5 and not graph.has_edge((x1 - 0.5, y1 + 0.5), (x1 - 0.5, y1 - 0.5)):
+                open_flag = True
+            if x1 == 5.5 and not graph.has_edge((x1 + 0.5, y1 + 0.5), (x1 + 0.5, y1 - 0.5)):
+                open_flag = True
+            if y1 == 5.5 and not graph.has_edge((x1 - 0.5, y1 + 0.5), (x1 + 0.5, y1 + 0.5)):
+                open_flag = True
+            if y1 == 0.5 and not graph.has_edge((x1 - 0.5, y1 - 0.5), (x1 + 0.5, y1 - 0.5)):
+                open_flag = True
+        if open_flag:
+            open_clusters.append(cluster)
+        else:
+            closed_clusters.append(cluster)
+    
     return open_clusters, closed_clusters
 
-def open_component_check(graph) :
-    checking_paths(graph)
-    if any(len(cluster) > 2 for cluster in open_clusters) : 
-        return False
-    else : 
-        return True
-print(open_component_check(G))
+def open_component_check(graph):
+    inverse = create_inverse_graph(graph)
+    open_clusters, _ = checking_paths(graph, inverse)
+    return not any(len(cluster) > 2 for cluster in open_clusters)
 
-def closed_component_check(graph) :
-    checking_paths(graph)
-    if any(len(cluster) > 7 for cluster in closed_clusters) : 
-        return False
-    else : 
-        return True
-print(closed_component_check(G))
+def closed_component_check(graph):
+    inverse = create_inverse_graph(graph)
+    _, closed_clusters = checking_paths(graph, inverse)
+    return not any(len(cluster) > 7 for cluster in closed_clusters)
 
-def maze_criteria(graph):
-    if nx.is_connected(graph) and nx.number_of_edges(graph) < 46 and outer_edge_count(graph) < 19 and open_component_check(graph) == True and closed_component_check(graph) == True:
+def maze_criteria(graph, max_edges=56, max_outer_edges=19):
+    if nx.is_connected(graph) and nx.number_of_edges(graph) > max_edges and outer_edge_count(graph) < max_outer_edges and open_component_check(graph) == True and closed_component_check(graph) == True:
         return True
     else :
         return False
-
-pos2 = {(x, y): (x, y) for x, y in G2.nodes()}
-
-# merge the two graphs
-pos = {node: node for node in G.nodes()}
-merged_graph = nx.compose(G, G2)
-
-# Combine positions for the merged graph
-combined_pos = pos.copy()
-combined_pos.update(pos2)
-
-# Define different colours and sizes for the two graphs
-colors_g = ['blue' for node in G.nodes()]
-sizes_g = [300 for node in G.nodes()]
-colors_g2 = ['red' for node in G2.nodes()]
-sizes_g2 = [100 for node in G2.nodes()]
-combined_colors = colors_g + colors_g2
-combined_sizes = sizes_g + sizes_g2
-
-# Draw the merged graph
-nx.draw(merged_graph, combined_pos, with_labels=True, node_color=combined_colors, node_size=combined_sizes, font_size=10)
-plt.show()
 
 # calculate maze fitness
 def get_maze_fitness(graph):
@@ -165,19 +112,21 @@ def get_maze_fitness(graph):
     correlation_coefficient = np.corrcoef(euclidean_distances, geodesic_distances)[0, 1]
     return 1 - abs(correlation_coefficient)
 
-
-# Create empty list to store successful mazes (e.g. trying to get n=100)
-# While len(list) < 100, keep generating random mazes and storing them in the list (if it passes the criteria)
-# List comphrension to get the fitness of each maze in the list
-# generate defined number of random mazes
 def generate_mazes():
-    for i in range(num_graphs):
-        maze = random_maze(G)
+    successful_mazes = []
+    while len(successful_mazes) < 100:
+        maze = random_maze()
+        print("Generated a new maze with " + str(nx.number_of_edges(maze)) + " edges.")
+        inverse_maze = create_inverse_graph(maze)
+        checking_paths(maze, inverse_maze)
         if maze_criteria(maze) == True:
-            maze
-            store_graph(maze, f'maze_{i}.pkl')
-            
-# draw the mazes
-nx.draw(G, pos, with_labels=True, node_size=800, font_size=10)
-plt.show()
+            successful_mazes.append(maze)
+            print(f"Created success: [{len(successful_mazes)}]")
+        else:
+            print("Maze failed criteria, trying again...")
+            print(f"Created: [{len(successful_mazes)}]")
+            continue
+    print("Finished generating mazes.")
+    print(successful_mazes)
 
+    
